00ea631f0dbf191bddd09f5bf86733c6345eb3bd
sirkhancision <jsantiago12tone@gmail.com>
2023-06-11 02:35:44 -0300

    Reformat code, handle exceptions and enhance logic in main file

--- main.py
+++ main.py
@@ -1,53 +1,40 @@
 #!/usr/bin/env python3
 
 import sys
-import RHEPY_lang as lang
-import constants as consts
-from colorama import Fore, init
+from constants import ENGLISH, EXPLANATION, INSTRUCTIONS, PORTUGUESE, TEST
+from colorama import Fore
+from RHEPY_lang import (print_instructions, print_post_greeting, print_result,
+                        print_result_explanation)
 from statements import print_statements
-
-# initialize colorama
-init()
 
 
 def print_greeting():
     """
-    Prints the first line of text, with or without the enneagram symbol ASCII art
+    Prints the first line of text, with or without the enneagram symbol
+    ASCII art
     """
-    display_symbol = True
-
-    for arg in sys.argv:
-        if arg == "--no-symbol":
-            display_symbol = False
+    display_symbol = "--no-symbol" not in sys.argv
 
     if display_symbol:
-        enneagram_symbol = open("enneagram_symbol.txt", "r")
-        print(Fore.CYAN + enneagram_symbol.read() + Fore.RESET)
-        enneagram_symbol.close()
+        with open("enneagram_symbol.txt", "r") as enneagram_symbol:
+            print(Fore.CYAN + enneagram_symbol.read() + Fore.RESET)
 
-    print(
-        "RHEPY v1.0\n"
-        "RHEPY is made by " + Fore.GREEN + "sirkhancision" + Fore.RESET + "\n"
-        "All rights are reserved to the respective owners of RHETI\n"
-    )
+    print("RHEPY v1.5\n"
+          "RHEPY is made by " + Fore.GREEN + "sirkhancision" + Fore.RESET +
+          "\n"
+          "All rights are reserved to the respective owners of RHETI\n")
 
 
 def language_choice():
     """
     Select the language of the text displayed
     """
-    print(
-        "Select the language of the test:\n"
-        "(select a different option to exit)\n"
-        + Fore.RED
-        + "[1] English\n"
-        + Fore.GREEN
-        + "[2] Portuguese"
-        + Fore.RESET
-    )
+    print("Select the language of the test:\n"
+          "(select a different option to exit)\n" + Fore.RED +
+          "[1] English\n" + Fore.GREEN + "[2] Portuguese" + Fore.RESET)
 
     language = input()
-    if language != consts.ENGLISH and language != consts.PORTUGUESE:
+    if language != ENGLISH and language != PORTUGUESE:
         print("Exiting...")
         sys.exit(0)
 
@@ -60,45 +47,25 @@ def test_choice(language):
     instructions, go directly to the test, or read how the results of the
     test are calculated
     """
-    match language:
-        case consts.ENGLISH:
-            print(
-                Fore.RED
-                + "[1] "
-                + Fore.GREEN
-                + "Instructions\n"
-                + Fore.RED
-                + "[2] "
-                + Fore.GREEN
-                + "The RHEPY test\n"
-                + Fore.RED
-                + "[3] "
-                + Fore.GREEN
-                + "Explain how the results are calculated"
-                + Fore.RESET
-            )
-            choice = input()
-        case consts.PORTUGUESE:
-            print(
-                Fore.RED
-                + "[1] "
-                + Fore.GREEN
-                + "Instruções\n"
-                + Fore.RED
-                + "[2] "
-                + Fore.GREEN
-                + "O teste RHEPY\n"
-                + Fore.RED
-                + "[3] "
-                + Fore.GREEN
-                + "Explicar como os resultados são calculados"
-                + Fore.RESET
-            )
-            choice = input()
-        case _:
-            print("Invalid language choice")
-            sys.exit(1)
-    return choice
+    choices = {
+        ENGLISH: (Fore.RED + "[1] " + Fore.GREEN + "Instructions\n" +
+                  Fore.RED + "[2] " + Fore.GREEN + "The RHEPY test\n" +
+                  Fore.RED + "[3] " + Fore.GREEN + "Explain how the results "
+                  "are calculated" + Fore.RESET),
+        PORTUGUESE: (Fore.RED + "[1] " + Fore.GREEN + "Instruções\n" +
+                     Fore.RED + "[2] " + Fore.GREEN + "O teste RHEPY\n" +
+                     Fore.RED + "[3] " + Fore.GREEN + "Explicar como os "
+                     "resultados são calculados" + Fore.RESET)
+    }
+
+    choices_output = choices.get(language)
+    if choices_output:
+        print(choices_output)
+
+        choice = input()
+        return choice
+    else:
+        raise ValueError("Invalid language choice")
 
 
 def main():
@@ -118,20 +85,26 @@ def main():
 
     print_greeting()
     language = language_choice()
-    lang.print_post_greeting(language)
 
-    while True:
-        choice = test_choice(language)
-        match choice:
-            case consts.INSTRUCTIONS:
-                lang.print_instructions(language)
-            case consts.EXPLANATION:
-                lang.print_result_explanation(language)
-            case consts.TEST:
+    try:
+        print_post_greeting(language)
+
+        while True:
+            choice = test_choice(language)
+
+            if choice == INSTRUCTIONS:
+                print_instructions(language)
+            elif choice == EXPLANATION:
+                print_result_explanation(language)
+            elif choice == TEST:
                 break
 
-    print_statements(language, enneagram_types)
-    lang.print_result(language, enneagram_types)
+        print_statements(language, enneagram_types)
+
+        print_result(language, enneagram_types)
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
